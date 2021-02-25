7b6fed35b51348dd3cf6d2f0818a513ef9af09d2
Saoud <saoud.b94@gmail.com>
2021-02-25 18:06:01 +0100

    Add HomeScreen & Product & Rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <>
       <Header />
-      <main className='py-3'>
+      <main className='my-3'>
         <Container>
-          <h1>Welcome to MyShop!</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
