a661ac61a33e82031e567c9de79a80ae57891a54
Saoud <saoud.b94@gmail.com>
2021-04-04 16:17:49 +0200

    Redirect http to https

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,19 @@ connectDB()
 
 const app = express()
 
+/* Redirect http to https */
+app.get('*', function (req, res, next) {
+  if (
+    'https' !== req.headers['x-forwarded-proto'] &&
+    'production' === process.env.NODE_ENV
+  ) {
+    res.redirect('https://' + req.hostname + req.url)
+  } else {
+    // Continue to other routes if we're not redirecting
+    next()
+  }
+})
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
